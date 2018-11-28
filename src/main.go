90e914d452b3c82cf6e8801ed28ca69bf3b03873
vagrant <vagrant@momentum56.test>
2018-11-28 14:53:36 -0500

    More progress

--- src/main.go
+++ src/main.go
@@ -5,50 +5,24 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"request"
 )
 
-/*
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-var people []Person
-*/
-
-//
-// Entry point
-//
 func main() {
-	//people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	//people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	//people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
-
 	router := mux.NewRouter()
-	router.HandleFunc("/multisite-create", GetPeople).Methods("GET")
-	/*router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")*/
+	router.HandleFunc("/multisite/create", MultiSiteCreate).Methods("GET")
+	router.HandleFunc("/multisite/update", MultiSiteUpdate).Methods("GET")
 	log.Print("Started server on port 8000");
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func GetPeople(w http.ResponseWriter, r *http.Request) {
-	//json.NewEncoder(w).Encode(people)
-	/*data, err := json.Marshal(NewParameterBag(r));
-
-	if (err != nil) {
-		log.Fatal(err)
-	}*/
-
-	json.NewEncoder(w).Encode(NewParameterBag(r))
+func MultiSiteCreate(w http.ResponseWriter, r *http.Request) {
+	respond(w, request.NewParameterBag(r))
 }
-func GetPerson(w http.ResponseWriter, r *http.Request) {}
-func CreatePerson(w http.ResponseWriter, r *http.Request) {}
-func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
+func MultiSiteUpdate(w http.ResponseWriter, r *http.Request) {}
+
+func respond(w http.ResponseWriter, data interface{})  {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(request.All())
+}
---
+++ src/request/parameterBag.go
@@ -0,0 +1,107 @@
+package request
+
+import (
+	"net/http"
+	"strings"
+	"strconv"
+	"utils"
+	"fmt"
+)
+
+const SITE_ID_JUSTFLY   = 1
+const SITE_ID_FLIGHTHUB = 4
+const CURRENCY_CODE_USD = "USD"
+const CURRENCY_CODE_CAD = "CAD"
+
+var _parameterBag       *ParameterBag
+var _request 		*http.Request
+var _siteId 		int
+var _otherSiteId 	int
+var _currency 		string
+
+type ParameterBag struct {
+	SiteId        	int    	`json:"site_id"`
+	OtherSiteId    	int	`json:"other_site_id"`
+	Currency    	string	`json:"currency"`
+}
+
+/**
+ * Constructor
+ */
+func NewParameterBag(r *http.Request) *ParameterBag {
+	_request      = r
+	_siteId       = getSiteId();
+	_otherSiteId  = getOtherSiteId();
+	_currency     = getCurrency();
+	_parameterBag = &ParameterBag{
+		SiteId: 	_siteId,
+		OtherSiteId:	_otherSiteId,
+		Currency: 	_currency,
+	}
+
+	return _parameterBag
+}
+
+/**
+ * Get all parameters
+ * @return ParameterBag
+ */
+func All() (*ParameterBag) {
+	return _parameterBag
+}
+
+/**
+ * Get a request param
+ *
+ * @param string - request variable name
+ * @param string (optional) - default value
+ * @return string
+ */
+func getRequestParam(paramName, defaultValue string) (string) {
+	value := strings.Join(_request.URL.Query()[paramName], "")
+
+	if (value == "" && defaultValue != "") {
+		value = defaultValue
+	}
+
+	return fmt.Sprintf("%v", value)
+}
+
+/**
+ * Get the site id
+ *
+ * @return int
+ */
+func getSiteId() (_siteId int) {
+	_siteId, err := strconv.Atoi(getRequestParam("site_id", ""))
+
+	if (err != nil || _siteId == 0) {
+		_siteId = SITE_ID_JUSTFLY;
+	}
+
+	exists, _ := utils.InArray(_siteId, []int{SITE_ID_JUSTFLY, SITE_ID_FLIGHTHUB})
+	if (exists == false) {
+		_siteId = SITE_ID_JUSTFLY;
+	}
+
+	return _siteId
+}
+/**
+ * Get Other site id
+ *
+ * @return int
+ */
+func getOtherSiteId() (int) {
+	return map[bool]int{true: SITE_ID_FLIGHTHUB, false: SITE_ID_JUSTFLY} [_siteId == SITE_ID_JUSTFLY]
+}
+
+/**
+ * Get the request currency
+ *
+ * @return string
+ */
+func getCurrency() (string) {
+	var currency string = getRequestParam("currency", CURRENCY_CODE_CAD);
+
+	return map[bool]string{true: currency, false: CURRENCY_CODE_CAD} [currency == CURRENCY_CODE_USD]
+}
